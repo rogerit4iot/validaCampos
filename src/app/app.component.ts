89769348e2e433ba842b313906ff9dc8de1103d7
Roger Gonzalez <roger.gonzalez@It4iot.com>
2020-04-24 09:13:14 -0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  form : FormGroup;
+
+  ngOnInit(): void {
+   this.form =  this._fb.group({
+        num1: ['', [Validators.required]],
+        num2: ['',[Validators.required]],
+        num3: ['',[Validators.required]],
+        num4: ['',[Validators.required]]
+    })
+  }
   title = 'teste-validacao';
+
+  constructor(private _fb: FormBuilder){
+
+  }
 }
